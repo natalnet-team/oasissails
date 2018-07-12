43d1f53d5bb8efc5f8b9477eb3a489bc3af7c619
bcq <bcq.cpp@gmail.com>
2018-07-12 15:54:01 -0300

    Homepage usuario | Quase lá

--- config/routes.js
+++ config/routes.js
@@ -35,6 +35,7 @@ module.exports.routes = {
   'GET /account/profile':    { action: 'account/view-edit-profile' },
   'GET /sobre':              { action: 'view-sobre' },
   'GET /editor':             { action: 'sgddedu/view-editor' },
+  'GET /homeusuario':        { view: 'pages/usuario/homepageusuario'},
 
 
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
