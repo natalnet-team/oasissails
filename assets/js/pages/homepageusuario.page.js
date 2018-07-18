73a1bd8611361934dd348632d19548760d5b9b82
bcq <bcq.cpp@gmail.com>
2018-07-18 20:31:03 -0300

    dashboard usuario | js em andamento

--- assets/js/pages/homepageusuario.page.js
+++ assets/js/pages/homepageusuario.page.js
@@ -14,13 +14,42 @@ parasails.registerPage('homepageusuario', {
     
     _.extend(this, SAILS_LOCALS);
   },
-  mounted: async function() {   
-    //...
+  mounted: async function () {
+    $(document).ready(function () {
+      $("#myCarousel-1").on("slide.bs.carousel", function (e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 4;
+        var totalItems = $(".carousel-item").length;
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+          var it = itemsPerSlide - (totalItems - idx);
+          for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "left") {
+              $(".carousel-item")
+                .eq(i)
+                .appendTo(".carousel-inner");
+            }
+            else {
+              $(".carousel-item")
+                .eq(0)
+                .appendTo($(this).find(".carousel-inner"));
+            }
+          }
+        }
+      });
+    });
+  },
+  get mounted() {
+    return this._mounted;
+  },
+  set mounted(value) {
+    this._mounted = value;
   },
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    //…
+        
   }
 });
