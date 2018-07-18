e0eab3885cd14de4b237ff9e572788e79838a697
bcq <bcq.cpp@gmail.com>
2018-07-18 18:43:05 -0300

    dashboard usuario | recomeçando do zero

--- assets/js/pages/homepageusuario.page.js
+++ assets/js/pages/homepageusuario.page.js
@@ -14,34 +14,9 @@ parasails.registerPage('homepageusuario', {
     
     _.extend(this, SAILS_LOCALS);
   },
-  mounted: async function() {
-    $(document).ready(function() {
-      $("#myCarousel").on("slide.bs.carousel", function(e) {
-        var $e = $(e.relatedTarget);
-        var idx = $e.index();
-        var itemsPerSlide = 5;
-        var totalItems = $(".carousel-item").length;
-    
-        if (idx >= totalItems - (itemsPerSlide - 1)) {
-          var it = itemsPerSlide - (totalItems - idx);
-          for (var i = 0; i < it; i++) {
-            // append slides to end
-            if (e.direction == "left") {
-              $(".carousel-item")
-                .eq(i)
-                .appendTo(".carousel-inner");
-            } else {
-              $(".carousel-item")
-                .eq(0)
-                .appendTo($(this).find(".carousel-inner"));
-            }
-          }
-        }
-      });
-    });
-    
+  mounted: async function() {   
+    //...
   },
-
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
