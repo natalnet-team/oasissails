01fc5b45dcea180a8559cb6d8e53585b1a981f5b
bcq <bcq.cpp@gmail.com>
2018-07-18 14:56:15 -0300

    Refazendo dashboard usuario

--- assets/js/pages/homepageusuario.page.js
+++ assets/js/pages/homepageusuario.page.js
@@ -11,10 +11,35 @@ parasails.registerPage('homepageusuario', {
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
   beforeMount: function() {
     // Attach any initial data from the server.
+    
     _.extend(this, SAILS_LOCALS);
   },
   mounted: async function() {
-    //…
+    $(document).ready(function() {
+      $("#myCarousel").on("slide.bs.carousel", function(e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 5;
+        var totalItems = $(".carousel-item").length;
+    
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+          var it = itemsPerSlide - (totalItems - idx);
+          for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "left") {
+              $(".carousel-item")
+                .eq(i)
+                .appendTo(".carousel-inner");
+            } else {
+              $(".carousel-item")
+                .eq(0)
+                .appendTo($(this).find(".carousel-inner"));
+            }
+          }
+        }
+      });
+    });
+    
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
