007e9ca4eb2a9979f895b5b102d6b915e5f7b00f
Lucas <lucasvieira07@hotmail.com>
2018-08-01 11:30:06 -0300

    att

--- assets/js/pages/sendgame.page.js
+++ assets/js/pages/sendgame.page.js
@@ -9,6 +9,16 @@ parasails.registerPage('sendgame', {
     // For tracking client-side validation errors in our form.
     // > Has property set to `true` for each invalid property in `formData`.
     formErrors: { /* … */ },
+
+    formRules: {
+      nome: {required: true},
+      autor: {required: true},
+      disciplina: {required: true},
+      descricao: {required: true},
+      serie: {required: true},
+      conteudo: {required: true},
+      game: {required: true}
+    },
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -20,65 +30,35 @@ parasails.registerPage('sendgame', {
   },
   mounted: async function() {
     //…
-    
+    $('input:file').change(function (e){
+      console.log("tipo: ", this.files[0].type);
+      var Ext_permitidas = ['rar', 'zip'];
+      var Ext_file = this.files[0].value.split('.').pop();
+
+      if(typeof Ext_permitidas.find(function(ext){ return Ext_file == ext;}) == 'undefined'){
+        console.log("Extensão ", Ext_file, " invalida");
+      }else{
+        console.log(Ext_file, " OK!");
+      }
+    });
   },
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    //…
-    handleParsingForm: function() {
-      // Clear out any pre-existing error messages.
-      this.formErrors = {};
- 
-      var argins = this.formData;
+    //… 
 
-      if(!argins.nome) {
-        this.formErrors.nome = true;
-      }
+    
 
-      if(!argins.autor) {
-        this.formErrors.autor = true;
-      }
-
-      if(!argins.disciplina) {
-        this.formErrors.disciplina = true;
-      }
-
-      if(argins.descricao) {
-        this.formErrors.descricao = true;
-      }
-
-      if(!argins.serie) {
-        this.formErrors.serie = true;
-      }
-
-      if(!argins.conteudo) {
-        this.formErrors.conteudo = true;
-      }
-
-      if(!argins.game) {
-        this.formErrors.game = true;
-      }
-      // If there were any issues, they've already now been communicated to the user,
-      // so simply return undefined.  (This signifies that the submission should be
-      // cancelled.)
-      if (Object.keys(this.formErrors).length > 0) {
-        return;
-      }
-
-      return argins;
-    },
-
-    Exttest: function(input){
-      var Ext_permtidas = ['rar', 'zip'];
-      var Ext_file = input.value.split('.').pop();
-
-      if(typeof Ext_permitidas.find(function(ext){ return Ext_file == ext;}) == 'undefined'){
-        sails.log.debug('extensão'+EXt_file+' invalida');
-      }else{
-        sails.log.debug(Ext_file+' OK!');
-      }
-    }
+    //Exttest: function(){
+    //  var Ext_permitidas = ['rar', 'zip'];
+    //  var Ext_file = this.files[0].value.split('.').pop();
+//
+    //  if(typeof Ext_permitidas.find(function(ext){ return Ext_file == ext;}) == 'undefined'){
+    //    sails.log.debug('extensão '+EXt_file+' invalida');
+    //  }else{
+    //    sails.log.debug(Ext_file+' OK!');
+    //  }
+    //}
   }
 });
